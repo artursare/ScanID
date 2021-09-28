b327e0cc2b455a346e6d4f8d0a170d8846f80649
Arturs Are <arearturs@gmail.com>
2021-09-28 14:27:00 +0300

    Initial Commit

---
+++ ScanID/ScanIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanIDApp.swift
+//  ScanID
+//
+//  Created by Artūrs Āre on 28/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScanIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
